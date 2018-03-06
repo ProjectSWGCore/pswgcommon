285fa5fd95374a38459b84f9550abadba8ab86b1
Mads Boddum <madsboddum@outlook.dk>
2018-03-06 20:54:41 +0100

    Added guild packets

--- src/main/java/com/projectswg/common/network/packets/PacketType.java
+++ src/main/java/com/projectswg/common/network/packets/PacketType.java
@@ -141,6 +141,8 @@ import com.projectswg.common.network.packets.swg.zone.chat.ConGenericMessage;
 import com.projectswg.common.network.packets.swg.zone.chat.VoiceChatStatus;
 import com.projectswg.common.network.packets.swg.zone.combat.GrantCommandMessage;
 import com.projectswg.common.network.packets.swg.zone.deltas.DeltasMessage;
+import com.projectswg.common.network.packets.swg.zone.guild.GuildRequestMessage;
+import com.projectswg.common.network.packets.swg.zone.guild.GuildResponseMessage;
 import com.projectswg.common.network.packets.swg.zone.insertion.ChatRoomList;
 import com.projectswg.common.network.packets.swg.zone.insertion.ChatServerStatus;
 import com.projectswg.common.network.packets.swg.zone.insertion.CmdStartScene;
@@ -242,6 +244,8 @@ public enum PacketType {
 	EXPERTISE_REQUEST_MESSAGE					(ExpertiseRequestMessage.CRC,		ExpertiseRequestMessage.class),
 	CHANGE_ROLE_ICON_CHOICE						(ChangeRoleIconChoice.CRC,			ChangeRoleIconChoice.class),
 	SHOW_LOOT_BOX								(ShowLootBox.CRC,					ShowLootBox.class),
+	GUILD_REQUEST_MESSAGE						(GuildRequestMessage.CRC,			GuildRequestMessage.class),
+	GUILD_RESPONSE_MESSAGE						(GuildResponseMessage.CRC,			GuildResponseMessage.class),
 	
 		// Chat
 		CHAT_CREATE_ROOM						(ChatCreateRoom.CRC,				ChatCreateRoom.class),
