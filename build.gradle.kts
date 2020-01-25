a99034ea151773706c1e00d153b76e6af60f1957
Josh Larson <joshlarson2015@gmail.com>
2020-01-25 15:01:16 -0500

    Changed build script to kotlin's DSL

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	idea
+	java
+	id("org.jetbrains.kotlin.jvm")
+	id("org.javamodularity.moduleplugin")
+}
+
+idea {
+	targetVersion = "12.0.1"
+    module {
+        inheritOutputDirs = true
+    }
+}
+
+repositories {
+	jcenter()
+}
+
+sourceSets {
+	main {
+		dependencies {
+			api(group="me.joshlarson", name="jlcommon", version="1.9.1")
+			api(group="org.bouncycastle", name="bcprov-jdk15on", version="1.60")
+			api(group="org.mongodb", name="mongodb-driver-sync", version="3.10.2")
+		}
+	}
+	test {
+		dependencies {
+			implementation(group="junit", name="junit", version="4.12")
+		}
+	}
+}
