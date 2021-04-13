b55d2295cf913ece4a229a83aa5cd2c438c6e34a
Josh-Larson <joshlarson2015@gmail.com>
2021-04-13 07:02:44 -0500

    Improved JDK compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,19 @@
 plugins {
 	idea
-	java
-	id("org.jetbrains.kotlin.jvm")
-	id("org.javamodularity.moduleplugin")
+	`java-library`
+	kotlin("jvm")
+}
+
+val javaVersion: String by ext
+val javaMajorVersion: String by ext
+val kotlinTargetJdk: String by ext
+
+java {
+	modularity.inferModulePath.set(true)
 }
 
 idea {
-	targetVersion = "12.0.1"
+	targetVersion = javaVersion
     module {
         inheritOutputDirs = true
     }
@@ -18,15 +25,26 @@ repositories {
 
 sourceSets {
 	main {
+        java.outputDir = File(java.outputDir.toString().replace("\\${File.separatorChar}java", ""))
+		
 		dependencies {
-			api(group="me.joshlarson", name="jlcommon", version="1.9.1")
+			implementation(group="org.jetbrains", name="annotations", version="20.1.0")
+			api(group="me.joshlarson", name="jlcommon", version="1.9.2")
 			api(group="org.bouncycastle", name="bcprov-jdk15on", version="1.60")
 			api(group="org.mongodb", name="mongodb-driver-sync", version="3.10.2")
 		}
 	}
 	test {
 		dependencies {
+			implementation(kotlin("stdlib"))
 			implementation(group="junit", name="junit", version="4.12")
 		}
 	}
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+	kotlinOptions {
+		jvmTarget = kotlinTargetJdk
+	}
+	destinationDir = sourceSets.main.get().java.outputDir
 }
