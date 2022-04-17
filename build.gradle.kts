6aa01a54151fc1a5bf375c9aeaad218450f3ec56
Ziggy <ziggyswg@outlook.com>
2022-04-17 23:06:08 +0200

    Changes to dependencies so they are compatible with holocore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,49 @@
 plugins {
 	idea
-	java
-	id("org.jetbrains.kotlin.jvm")
-	id("org.javamodularity.moduleplugin")
+	`java-library`
+	kotlin("jvm")
+}
+
+val javaVersion: String by ext
+val javaMajorVersion: String by ext
+val kotlinTargetJdk: String by ext
+
+java {
+	modularity.inferModulePath.set(true)
 }
 
 idea {
-	targetVersion = "12.0.1"
-    module {
-        inheritOutputDirs = true
-    }
+	targetVersion = javaMajorVersion
+	module {
+		inheritOutputDirs = true
+	}
 }
 
 repositories {
+	maven("https://dev.joshlarson.me/maven2")
 	jcenter()
 }
 
 sourceSets {
 	main {
 		dependencies {
-			api(group="me.joshlarson", name="jlcommon", version="1.9.1")
-			api(group="org.bouncycastle", name="bcprov-jdk15on", version="1.60")
+			implementation(group = "org.jetbrains", name = "annotations", version = "20.1.0")
+			api(group = "me.joshlarson", name = "jlcommon", version = "1.10.0")
+			api(group = "org.bouncycastle", name = "bcprov-jdk15on", version = "1.60")
 			implementation(kotlin("stdlib"))
-			implementation(group="org.mongodb", name="mongodb-driver-sync", version="3.12.2")
+			implementation(group = "org.mongodb", name = "mongodb-driver-sync", version = "3.12.2")
 		}
 	}
 	test {
 		dependencies {
-			implementation(group="junit", name="junit", version="4.12")
+			implementation(kotlin("stdlib"))
+			implementation(group = "junit", name = "junit", version = "4.12")
 		}
+	}
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+	kotlinOptions {
+		jvmTarget = kotlinTargetJdk
 	}
 }
