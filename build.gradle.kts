803ccbb40d65bed6f05c7cf1e7ea493242433dc9
Josh-Larson <joshlarson2015@gmail.com>
2022-07-19 19:31:55 -0500

    Fixed test dependencies leaking into pswgcommon and fixed bouncycastle version for jdk 1.8+

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ sourceSets {
 	main {
 		dependencies {
 			api(group="org.jetbrains", name="annotations", version="20.1.0")
-			api(group="me.joshlarson", name="jlcommon", version="1.10.0")
-			api(group="org.bouncycastle", name="bcprov-jdk15on", version="1.60")
+			api(group="me.joshlarson", name="jlcommon", version="1.10.1")
+			api(group="org.bouncycastle", name="bcprov-jdk18on", version="1.71")
 			implementation(kotlin("stdlib"))
 			implementation(group="org.mongodb", name="mongodb-driver-sync", version="3.12.2")
 		}
