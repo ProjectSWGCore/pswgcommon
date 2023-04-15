8433fded87539fcc897a68db827770388ae5189c
Ziggy <ziggyswg@outlook.com>
2023-04-15 17:53:30 +0200

    Deprecated RelationalServerData and RelationalServerFactory

--- src/main/java/com/projectswg/common/data/info/RelationalServerFactory.java
+++ src/main/java/com/projectswg/common/data/info/RelationalServerFactory.java
@@ -1,5 +1,5 @@
 /***********************************************************************************
- * Copyright (c) 2018 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
@@ -35,6 +35,10 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @deprecated use SdbLoader instead in Holocore.
+ */
+@Deprecated
 public class RelationalServerFactory {
 	
 	private static final RelationalServerFactory INSTANCE = new RelationalServerFactory();
