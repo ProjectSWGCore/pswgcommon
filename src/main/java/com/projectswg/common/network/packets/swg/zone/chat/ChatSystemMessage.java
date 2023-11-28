4fcb6279b46c9f90d50289e80e837dfeb0e466d3
Ziggy <ziggyswg@outlook.com>
2023-11-28 16:43:25 +0100

    Added getters and setters to some packets, so can work with them in automated tests

--- src/main/java/com/projectswg/common/network/packets/swg/zone/chat/ChatSystemMessage.java
+++ src/main/java/com/projectswg/common/network/packets/swg/zone/chat/ChatSystemMessage.java
@@ -1,5 +1,5 @@
 /***********************************************************************************
- * Copyright (c) 2018 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
@@ -88,7 +88,15 @@ public class ChatSystemMessage extends SWGPacket {
 	public String getMessage() {
 		return message;
 	}
-	
+
+	public OutOfBandPackage getOob() {
+		return oob;
+	}
+
+	public SystemChatType getType() {
+		return type;
+	}
+
 	public enum SystemChatType {
 		PERSONAL	(0x00),
 		BROADCAST	(0x01),
--- src/main/java/com/projectswg/common/network/packets/swg/zone/server_ui/SuiEventNotification.java
+++ src/main/java/com/projectswg/common/network/packets/swg/zone/server_ui/SuiEventNotification.java
@@ -1,5 +1,5 @@
 /***********************************************************************************
- * Copyright (c) 2018 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
@@ -82,4 +82,20 @@ public class SuiEventNotification extends SWGPacket {
 	public List<String> getSubscribedToProperties() { return this.subscribedToProperties; }
 	public int getEventIndex() { return this.eventIndex; }
 	public int getUpdateCount() { return this.updateCount; }
+
+	public void setWindowId(int windowId) {
+		this.windowId = windowId;
+	}
+
+	public void setEventIndex(int eventIndex) {
+		this.eventIndex = eventIndex;
+	}
+
+	public void setUpdateCount(int updateCount) {
+		this.updateCount = updateCount;
+	}
+	
+	public void addSubscribedToProperty(String property) {
+		this.subscribedToProperties.add(property);
+	}
 }
